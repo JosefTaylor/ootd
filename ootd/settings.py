48f6dc4c232f5307cec2651e084a45ab234508f8
Josef Taylor <josef.taylor@gmail.com>
2022-11-07 09:55:19 -0500

    adding secret key as environment variable

--- ootd/settings.py
+++ ootd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju*#@+ju1sqhcsx+ubqwo!e6k%m$^u*t9-!rf=2+v_y4@c-_vw'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
