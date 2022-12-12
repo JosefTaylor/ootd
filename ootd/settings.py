e9a89b67ee70499d666a1ca6fd4fff3575ebf9b7
Josef Taylor <josef.taylor@gmail.com>
2022-12-12 15:25:42 -0500

    adding django-allauth@0.50.0

--- ootd/settings.py
+++ ootd/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
