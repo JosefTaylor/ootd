0457a5c1979e62659230f47916861cb800789473
Josef Taylor <josef.taylor@gmail.com>
2022-12-13 13:00:31 -0500

    adding nvm to bash to install node and npm

--- build.sh
+++ build.sh
@@ -4,6 +4,17 @@ set -o errexit
 
 poetry install
 
+#install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#Install Node and NPM
+nvm install 19
+nvm install-latest-npm
+
+#install Node dependencies
 npm install
 npm run build
 
