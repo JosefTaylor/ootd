b227abede4021d1264b1f1bf8c62b84b0ac87c76
Josef Taylor <josef.taylor@gmail.com>
2022-12-14 11:46:39 -0500

    removing main.js from gitignore

--- build.sh
+++ build.sh
@@ -4,19 +4,5 @@ set -o errexit
 
 poetry install
 
-#install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-#Install Node and NPM
-nvm install 19
-nvm install-latest-npm
-
-#install Node dependencies
-npm install
-npm run build
-
 python manage.py collectstatic --no-input
 python manage.py migrate
