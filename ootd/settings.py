c722cad7eb727106f5602355e6129a77081f7f20
Josef Taylor <josef.taylor@gmail.com>
2022-12-14 19:59:32 -0500

    removing CORS middleware, adding brotli to whitenoise

--- ootd/settings.py
+++ ootd/settings.py
@@ -64,15 +64,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware', 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.security.SecurityMiddleware',  # not in dj-rest-auth tutorial
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 
 MIDDLEWARE_CLASSES = MIDDLEWARE
