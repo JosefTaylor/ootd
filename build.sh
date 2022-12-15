cdde4e3796a3013252fca3f07a6088c925401ab0
Josef Taylor <josef.taylor@gmail.com>
2022-12-14 22:21:30 -0500

    install node and build main.js on deploy

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ set -o errexit
 
 poetry install
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# nvm install node
+nvm install node
 
-# npm install
-# npm run build
+npm install
+npm run build
 
 python manage.py collectstatic --no-input
 python manage.py migrate
