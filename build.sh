5f03e8237c0e49306674bca9b6bb141385d07746
Josef Taylor <josef.taylor@gmail.com>
2022-12-16 09:26:39 -0500

    no NVM - node is 19 already

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ poetry install
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install node 19
-nvm use 19
+# nvm install node 19
+# nvm use 19
 
 npm install
 npm run build
