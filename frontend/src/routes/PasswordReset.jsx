af99b0e58cea2459496c0bbf0c2980445bf170fe
Josef Taylor <josef.taylor@gmail.com>
2023-01-05 13:54:27 -0500

    code formatting

--- frontend/src/routes/PasswordReset.jsx
+++ frontend/src/routes/PasswordReset.jsx
@@ -1,61 +1,61 @@
 import React, { Component } from "react";
-import {API, GetCookie} from "../axiosApi.jsx";
+import { API } from "../axiosApi.jsx";
 // import axios from "axios";
 import Card from "../components/Card.jsx";
 
 export default class PasswordReset extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			email: "",
-			loginError: false,
-		}
-		this.handleChange = this.handleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      loginError: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+      loginError: false,
+    });
+  }
 
-	handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value,
-			loginError: false,
-		});
-	}
+  handleSubmit(event) {
+    event.preventDefault();
+    API.post("/dj-rest-auth/reset/", {
+      email: this.state.email,
+    })
+      .then((response) => {
+        this.props.onLogin();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
 
-	handleSubmit(event) {
-		event.preventDefault();
-		API.post("/dj-rest-auth/reset/",
-			{
-				email: this.state.email,
-			})
-			.then((response) => {
-				this.props.onLogin();
-			})
-				.catch((error) => {
-				throw error;
-			});
-	}
-
-	render() {
-		return (
-			<div className="wrapper pad-1 wd-small center">
-				<Card title="Reset my password">
-					<label htmlFor="email" hidden>Username</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						value={this.state.username}
-						onChange={this.handleChange}
-						placeholder="email"
-						required
-					/>					
-					<button onClick={this.handleSubmit}>
-						Submit
-					</button>
-					<div className='warning' hidden={!this.state.loginError}>That didn't work, please try again.</div>
-				</Card >
-			</div >
-		)
-	}
+  render() {
+    return (
+      <div className="wrapper pad-1 wd-small center">
+        <Card title="Reset my password">
+          <label htmlFor="email" hidden>
+            Username
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={this.state.username}
+            onChange={this.handleChange}
+            placeholder="email"
+            required
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+          <div className="warning" hidden={!this.state.loginError}>
+            That didnt work, please try again.
+          </div>
+        </Card>
+      </div>
+    );
+  }
 }
--- frontend/src/routes/Public.jsx
+++ frontend/src/routes/Public.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "../components/Card.jsx";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function Public(props) {
+export default function Public() {
   return (
     <div className="wrapper pad-1 wd-small center">
       <Card title="Welcome!">
-        <NavLink to="user/home">Get Started</NavLink>
+        <NavLink to="/home">Get Started</NavLink>
         <p>About</p>
       </Card>
     </div>
