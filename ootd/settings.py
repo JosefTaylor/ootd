d7a38aae39ab4d0540629ccb7c2a45413a4e86d5
Josef Taylor <josef.taylor@gmail.com>
2023-01-05 13:55:42 -0500

    more formatting

--- ootd/settings.py
+++ ootd/settings.py
@@ -18,69 +18,68 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = SECRET_KEY = os.environ.get(
-    'SECRET_KEY', default='your secret key')
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = "RENDER" not in os.environ
 
 if DEBUG:
-    ALLOWED_HOSTS = [] 
+    ALLOWED_HOSTS = []
 else:
     ALLOWED_HOSTS = [
-        os.environ.get('RENDER_EXTERNAL_HOSTNAME'),
-        os.environ.get('EXTERNAL_HOSTNAME'),
+        os.environ.get("RENDER_EXTERNAL_HOSTNAME"),
+        os.environ.get("EXTERNAL_HOSTNAME"),
     ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'closet.apps.ClosetConfig',
-    'frontend',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'dj_rest_auth',
+    "closet.apps.ClosetConfig",
+    "frontend",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "dj_rest_auth",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware', 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 MIDDLEWARE_CLASSES = MIDDLEWARE
 
-ROOT_URLCONF = 'ootd.urls'
+ROOT_URLCONF = "ootd.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ootd.wsgi.application'
+WSGI_APPLICATION = "ootd.wsgi.application"
 
 
 # Database
@@ -96,23 +95,25 @@ WSGI_APPLICATION = 'ootd.wsgi.application'
 #         conn_max_age=600
 #     )
 # }
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -120,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -132,24 +133,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': (
     #     'rest_framework.permissions.IsAuthenticated',
     # ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
     ),
 }
 
@@ -160,38 +161,38 @@ REST_AUTH_TOKEN_MODEL = None
 # CSRF
 if DEBUG:
     CSRF_TRUSTED_ORIGINS = [
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ]
     CSRF_COOKIE_SECURE = False
-    CSRF_COOKIE_SAMESITE = 'Lax'
+    CSRF_COOKIE_SAMESITE = "Lax"
 else:
     CSRF_TRUSTED_ORIGINS = [
-        os.environ.get('RENDER_EXTERNAL_URL'),
-        os.environ.get('EXTERNAL_URL'),
+        os.environ.get("RENDER_EXTERNAL_URL"),
+        os.environ.get("EXTERNAL_URL"),
     ]
     CSRF_COOKIE_SECURE = True
-    CSRF_COOKIE_SAMESITE = 'None'
-    SESSION_COOKIE_SAMESITE = 'None'
+    CSRF_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SAMESITE = "None"
 
 # # CORS
 if DEBUG:
     CORS_ALLOWED_ORIGINS = [
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ]
     CORS_ORIGIN_WHITELIST = [
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ]
 else:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('RENDER_EXTERNAL_URL'),
-        os.environ.get('EXTERNAL_URL')
+        os.environ.get("RENDER_EXTERNAL_URL"),
+        os.environ.get("EXTERNAL_URL"),
     ]
     CORS_ORIGIN_WHITELIST = [
-        os.environ.get('RENDER_EXTERNAL_URL'),
-        os.environ.get('EXTERNAL_URL')
+        os.environ.get("RENDER_EXTERNAL_URL"),
+        os.environ.get("EXTERNAL_URL"),
     ]
 
 
