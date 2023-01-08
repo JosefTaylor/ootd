a11844a24046dce53737ef2729138bb2db03f05d
Josef Taylor <josef.taylor@gmail.com>
2023-01-07 21:54:03 -0500

    Fixing New Garment issue, and checking for a null purchase price.

--- closet/models.py
+++ closet/models.py
@@ -29,7 +29,11 @@ class Fashionista(models.Model):
     def garments(self):
         return Garment.objects.filter(owner=self.user)
 
+    def garments(self):
+        return Garment.objects.filter(owner=self.user)
+
     def garment_wears(self):
+        return GarmentWear.objects.filter(wearer=self.user)
         return GarmentWear.objects.filter(wearer=self.user)
 
 
@@ -50,6 +54,7 @@ class Garment(models.Model):
             # Make sure purchase_date is in the past
             now = timezone.now().date()
             if self.purchase_date > now:
+                raise ValidationError(_("Purchase Date must be in the past."))
                 raise ValidationError(_("Purchase Date must be in the past."))
             # Set the pub_date for published items if it hasn't been set already.
             if self.purchase_date == None:
@@ -79,7 +84,13 @@ class GarmentWear(models.Model):
     def __str__(self):
         if self.wearer == self.garment.owner:
             return f"{self.wearer.username} wore their " + f"{self.garment.name}"
+            return f"{self.wearer.username} wore their " + f"{self.garment.name}"
         else:
+            return (
+                f"{self.wearer.username} wore "
+                + f"{self.garment.owner.username}'s "
+                + f"{self.garment.name}"
+            )
             return (
                 f"{self.wearer.username} wore "
                 + f"{self.garment.owner.username}'s "
@@ -109,6 +120,7 @@ class Outfit(models.Model):
     # style
     # dress_code
     # season
+    # list of garments?
     # list of garments?
 
     def __str__(self):
