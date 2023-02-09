1273c79e08f11c87acc7041d5576841eeb00f4fe
Josef Taylor <josef.taylor@gmail.com>
2023-02-09 13:27:51 -0500

    data table scrolls!!!!

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 export default function DataTable(props) {
   return (
-    <table id="data-table" className={props.className}>
-      {props.header ? (
-        <thead>
-          <tr>
-            {props.header.map((field, index) => (
-              <th key={index}>{field}</th>
-            ))}
-          </tr>
-        </thead>
-      ) : (
-        ""
-      )}
-      <tbody>{props.children}</tbody>
-    </table>
+    <div className="scroll">
+      <table id="data-table" className={props.className}>
+        {props.header ? (
+          <thead>
+            <tr>
+              {props.header.map((field, index) => (
+                <th key={index}>{field}</th>
+              ))}
+            </tr>
+          </thead>
+        ) : (
+          ""
+        )}
+        <tbody>{props.children}</tbody>
+      </table>
+    </div>
   );
 }
